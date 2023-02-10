425535078440df77e23b9e55df1be5f5d3ec9e27
João Pedro Thuler Lima <thuler_lima@hotmail.com>
2023-02-10 00:47:46 -0300

    feat: profile page created

--- src/components/ProfileCard/index.tsx
+++ src/components/ProfileCard/index.tsx
@@ -10,7 +10,21 @@ const TeamsCard = () => {
               <section className="section01">
                 <h2>Perfil de usuário</h2>          
               </section>
-          </Style.ProfileContainer>  
+              <section className="section02">
+                <section>
+                  <img src="https://avatars.githubusercontent.com/u/97922574?v=4"></img>
+                  <div>
+                    <div className="top">
+                      <h2>João Pedro Thuler Lima</h2>
+                      <Style.Gear/>{' '}
+                    </div>
+                    <p>thuler_lima@hotmail.com</p>
+                    <p>22 9990-9574</p>
+                    <p className="updateImg" onClick={()=>{}}>Upload de Imagem</p>
+                  </div>
+                </section>
+              </section>
+          </Style.ProfileContainer>
   );
 };
 
