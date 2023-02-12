63a9e2c178a1a2b4b568cdf5426a73424bc1f391
João Pedro Thuler Lima <thuler_lima@hotmail.com>
2023-02-12 17:43:23 -0300

    feat: created logic to add new project

--- src/components/ProjectsCard/index.tsx
+++ src/components/ProjectsCard/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import Api from "../../services/api";
 import * as Style from "./style";
@@ -12,6 +13,26 @@ const ProjectsCard = () => {
   const navigate = useNavigate()
   const [ openNewTeam, setOpenNewTeam ] = useState<boolean>(false)
   const [ allProjects, setAllProjects ] = useState<any>([])
+  const [ newTeam, setNewTeam ] = useState<string>()
+  const [ newTeamValue, setNewTeamValue ] = useState<string>()
+
+  const handleNewProject = () =>{
+    if(newTeam !== "" && newTeamValue !== ""){
+      Api.post("/project",
+      {
+        name: newTeam,
+        description: newTeamValue
+      }
+      )
+      .then(()=>{
+        handleAllProjects()
+        setOpenNewTeam(false)
+        toast.success("Projeto criado")
+      }).catch(()=>{
+        toast.error("Erro ao criar projeto")
+      })
+    }
+  }
 
   const handleAllProjects = () =>{
     Api.get("/project")
@@ -42,13 +63,13 @@ useEffect(()=>{
                 {openNewTeam && <section className="registerTeam newQuestion animate__animated animate__fadeInDownBig animate__delay-0.5s">
                   <div>
                     <p>Nome</p>
-                    <input type="text"></input>
+                    <input type="text" onChange={(e)=>setNewTeam(e.target.value)}></input>
                   </div>
                   <div>
                     <p >Decrição</p>
-                    <input type="text" className="inputDescription"></input>
+                    <input type="text" className="inputDescription" onChange={(e)=>setNewTeamValue(e.target.value)}></input>
                   </div>
-                  <p className="confirmNewProject" onClick={()=>{}}>Cadastrar</p>
+                  <p className="confirmNewProject" onClick={()=>{handleNewProject()}}>Cadastrar</p>
                 </section>}
                 <section className="allCards">
                 {allProjects && allProjects.map((element:any , index:number)=>{
@@ -56,7 +77,7 @@ useEffect(()=>{
                   <div key={index} className="card" onClick={()=>{
                     handleNext(element.id);
                     }}>
-                        <p>{`Projeto ${element.name}`}</p>
+                        <p>{`${index<9? `0${index+1}`: index+1} - ${element.name}`}</p>
                         <Style.Settings/>{" "}
                   </div>
                   )
